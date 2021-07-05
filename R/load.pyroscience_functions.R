f8600fb59b31f3d76a975f5756a8f75db62ffa33
hugomflavio <hmflavio92@gmail.com>
2021-07-04 21:44:32 -0400

    new function to patch NA values (undocumented)

--- R/load.pyroscience_functions.R
+++ R/load.pyroscience_functions.R
@@ -19,17 +19,17 @@ load.pyroscience.logger.file <- function(file, n.chamber = 1:4, date.format) {
   column.vector <- c(1, 2, 9, 5, 10, 6, 11, 7, 12, 8)[1:(2 + 2 * n.chamber)]
   column.names <- c("Date", "Time", "Temp.1", "Ox.1", "Temp.2", "Ox.2", "Temp.3", "Ox.3", "Temp.4", "Ox.4")[1:(2 + 2 * n.chamber)]
 
-  pyro.data <- utils::read.table(file, sep = "\t", skip = 12 + (2 * n.chamber), header = FALSE, strip.white = TRUE)
-  pyro.data <- pyro.data[, column.vector]
-  names(pyro.data) <- column.names
-  pyro.data$Date.Time <- paste(pyro.data$Date, pyro.data$Time)
-  pyro.data$Phase <- "F"
-  pyro.data[pyro.data == "---"] <- NA
+  pyro <- utils::read.table(file, sep = "\t", skip = 12 + (2 * n.chamber), header = FALSE, strip.white = TRUE)
+  pyro <- pyro[, column.vector]
+  names(pyro) <- column.names
+  pyro$Date.Time <- paste(pyro$Date, pyro$Time)
+  pyro$Phase <- "F"
+  pyro[pyro == "---"] <- NA
   
-  pyro.data <- pyro.data[, c(ncol(pyro.data) - 1, ncol(pyro.data), 3:(ncol(pyro.data)-2))]
+  pyro <- pyro[, c(ncol(pyro) - 1, ncol(pyro), 3:(ncol(pyro)-2))]
   
-  pyro.data$Date.Time <- as.POSIXct(pyro.data$Date.Time, format = paste(date.format, "%H:%M:%S"))
-  return(pyro.data)
+  pyro$Date.Time <- as.POSIXct(pyro$Date.Time, format = paste(date.format, "%H:%M:%S"))
+  return(pyro)
 }
 
 #' Load a respirometry data file originating from pyroscience's 'PyroScience Workbench' software.
@@ -53,9 +53,10 @@ load.pyroscience.workbench.file <- function(file, date.format, o2_from, o2_to) {
     stop("Could not find target file.", call. = FALSE)
 
   aux <- readLines(file, n = 100)
-  preamble <- sum(grepl("^#", aux))
-  last.chamber <- tail(aux[grepl("Ch.[1-4]\\] - Oxygen Sensor", aux)], 1)
-  n.chambers <- as.numeric(sub("\\D*(\\d+).*", "\\1", last.chamber))
+  preamble <- suppressWarnings(max(grep("^#", aux)))
+  
+  aux <- suppressWarnings(aux[grepl("Ch.[1-4]\\] - Oxygen Sensor", aux)])
+  n.chambers <- length(sub("\\D*(\\d+).*", "\\1", aux))
 
   rm(aux)
 
@@ -67,37 +68,97 @@ load.pyroscience.workbench.file <- function(file, date.format, o2_from, o2_to) {
     column.names <- c("Date", "Time", "Temp.1", "Ox.1", "Temp.2", "Ox.2", "Temp.3", "Ox.3", "Temp.4", "Ox.4")[1:(2 + 2 * n.chambers)]
   }
 
-  pyro.data <- as.data.frame(data.table::fread(file, sep = "\t", skip = preamble, strip.white = TRUE, tz = ""))
+  pyro <- as.data.frame(data.table::fread(file, sep = "\t", skip = preamble, strip.white = TRUE, tz = ""))
 
-  pyro.data <- pyro.data[, column.vector]
-  names(pyro.data) <- column.names
+  pyro <- pyro[, column.vector]
+  names(pyro) <- column.names
 
-  pyro.data$Date.Time <- paste(pyro.data$Date, pyro.data$Time)
-  pyro.data$Phase <- NA
-  pyro.data[pyro.data == "---"] <- NA
+  pyro$Date.Time <- paste(pyro$Date, pyro$Time)
+  pyro$Phase <- NA
+  pyro[pyro == "---"] <- NA
   
-  pyro.data <- pyro.data[, c(ncol(pyro.data) - 1, ncol(pyro.data), 3:(ncol(pyro.data)-2))]
+  pyro <- pyro[, c(ncol(pyro) - 1, ncol(pyro), 3:(ncol(pyro)-2))]
   
-  pyro.data$Date.Time <- as.POSIXct(pyro.data$Date.Time, format = paste(date.format, "%H:%M:%S"), tz = Sys.timezone())
+  pyro$Date.Time <- as.POSIXct(pyro$Date.Time, format = paste(date.format, "%H:%M:%S"), tz = Sys.timezone())
 
   # failsafe in case the file comes with a trailing NA line
-  if (all(is.na(pyro.data[nrow(pyro.data), ])))
-    pyro.data <- pyro.data[-nrow(pyro.data), ]
+  if (all(is.na(pyro[nrow(pyro), ])))
+    pyro <- pyro[-nrow(pyro), ]
 
   if (convert_o2) {
     for (i in 1:n.chambers) {
-      pyro.data[, paste0("Ox.", i)] <- 
-        respirometry::conv_o2(o2 = pyro.data[, paste0("Ox.", i)],
+      pyro[, paste0("Ox.", i)] <- 
+        respirometry::conv_o2(o2 = pyro[, paste0("Ox.", i)],
                               from = o2_from,
                               to = o2_to,
-                              temp = pyro.data[, paste0("Temp.", i)],
-                              sal = 0, atm_pres = pyro.data[, paste0("Pressure.", i)])
+                              temp = pyro[, paste0("Temp.", i)],
+                              sal = 0, atm_pres = pyro[, paste0("Pressure.", i)])
     }
-    pyro.data <- pyro.data[,!grepl("Pressure", colnames(pyro.data))]
+    pyro <- pyro[,!grepl("Pressure", colnames(pyro))]
   }
 
-  return(pyro.data)
+  if (any(is.na(pyro[,-1:-2])))
+    warning('NA values found in the data!', immediate. = TRUE, call. = FALSE)
+
+  return(pyro)
 }
+
+
+patch.NAs <- function(input, method = c('linear', 'before', 'after')) {
+  db <- input[,-1:-2]
+  logical_db <- apply(db, 2, function(x) is.na(x))
+  rle_list <- apply(logical_db, 2, rle)
+
+  capture <- lapply (1:ncol(db), function(coln) {
+    aux <- cumsum(rle_list[[coln]]$lengths)
+    breaks <- data.frame(Value = rle_list[[coln]]$values,
+                         Start = c(1, aux[-length(aux)] + 1),
+                         Stop = aux)
+
+    if (any(breaks$Value)) {
+      nas <- breaks[breaks$Value, ]
+      for (i in 1:nrow(nas)) {
+        if (method == 'linear') {
+          if (nas$Start[i] == 1) {
+            warning("NAs found at the start of a column. Using method = 'after' for this instance.", immediate. = TRUE, call. = FALSE)
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Stop[i] + 1, coln]
+          } 
+          else if (nas$Stop[i] == nrow(db)) {
+            warning("NAs found at the end of a column. Using method = 'before' for this instance.", immediate. = TRUE, call. = FALSE)
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Start[i] - 1, coln]
+          } 
+          else {
+            fake_values <- seq(from = db[nas$Start[i] - 1, coln],
+                               to = db[nas$Stop[i] + 1, coln],
+                               length.out = nas$Stop[i] - nas$Start[i] + 3) # 1 for the before value, 1 for the after, and 1 for the value that is eliminated by the subtraction
+            db[nas$Start[i]:nas$Stop[i], coln] <<- fake_values          
+          }
+        }
+        if (method == 'before') {
+          if (nas$Start[i] == 1) {
+            warning("NAs found at the start of a column. Using method = 'after' for this instance.", immediate. = TRUE, call. = FALSE)
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Stop[i] + 1, coln]
+          } 
+          else {
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Start[i] - 1, coln]
+          }
+        }
+        if (method == 'after') {
+          if (nas$Stop[i] == nrow(db)) {
+            warning("NAs found at the end of a column. Using method = 'before' for this instance.", immediate. = TRUE, call. = FALSE)
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Start[i] - 1, coln]
+          }
+          else {
+            db[nas$Start[i]:nas$Stop[i], coln] <<- db[nas$Stop[i] + 1, coln]            
+          }         
+        }
+      }
+    }
+  })
+
+  return(as.data.frame(cbind(input[,1:2], db)))
+}
+
 
 #' Confirm that the pyroscience input contains the expected columns
 #' 
