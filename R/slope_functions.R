43798107c2d9c9bb3a9fde45a3af93c9070ddc1a
hugomflavio <hmflavio92@gmail.com>
2024-04-06 14:06:33 -0400

    update calc_slopes to work with cycles instead of phases

--- R/slope_functions.R
+++ R/slope_functions.R
@@ -6,25 +6,25 @@
 #' @export
 #' 
 calc_slopes <- function(input) {
-  # The operation is done by phase and by_probe,
+  # The operation is done by cycle and by probe,
   # so the dataset is broken twice below
   by_probe <- split(input, input$probe)
 
   recipient <- lapply(by_probe, function(the_probe) {
-    by_phase <- split(the_probe, the_probe$phase)
-    by_phase <- by_phase[sapply(by_phase, nrow) > 0]
+    by_cycle <- split(the_probe, the_probe$cycle)
+    by_cycle <- by_cycle[sapply(by_cycle, nrow) > 0]
 
-    recipient <- lapply(by_phase, function(phase) {
-        output <- data.frame(probe = phase$probe[1],
-                             id = phase$id[1],
-                             mass = phase$mass[1],
-                             volume = phase$volume[1],
-                             date_time = phase$date_time[nrow(phase)],
-                             phase = phase$phase[1],
-                             cycle = phase$cycle[1],
-                             temp = mean(phase$temp))
+    recipient <- lapply(by_cycle, function(the_cycle) {
+        output <- data.frame(probe = the_cycle$probe[1],
+                             id = the_cycle$id[1],
+                             mass = the_cycle$mass[1],
+                             volume = the_cycle$volume[1],
+                             date_time = the_cycle$date_time[nrow(the_cycle)],
+                             phase = the_cycle$phase[1],
+                             cycle = the_cycle$cycle[1],
+                             temp = mean(the_cycle$temp, na.rm = TRUE))
 
-      if (all(is.na(phase$o2_cordelta))) {
+      if (all(is.na(the_cycle$o2_cordelta))) {
         output$slope_with_bg <- NA
         output$slope_cor <- NA
         output$se <- NA
@@ -35,9 +35,9 @@ calc_slopes <- function(input) {
         # to ask for a summary. Must drop units and reattach
         # them later until this is fixed.
         m_with_bg <- lm(as.numeric(o2_delta) ~ as.numeric(phase_time), 
-                        data = phase)
+                        data = the_cycle)
         m_cor <- lm(as.numeric(o2_cordelta) ~ as.numeric(phase_time),
-                        data = phase)
+                        data = the_cycle)
 
         output$slope_with_bg <- coef(m_with_bg)[2]
         output$slope_cor <- coef(m_cor)[2]
@@ -45,10 +45,10 @@ calc_slopes <- function(input) {
         output$r2 <- summary(m_cor)$r.squared
       }
       #make a dummy variable to let {units} figure the units out by itself
-      dummy <- phase$o2_delta[1] / phase$phase_time[1]
+      dummy <- the_cycle$o2_delta[1] / the_cycle$phase_time[1]
       units(output$slope_with_bg) <- units(dummy)
       
-      dummy <- phase$o2_cordelta[1] / phase$phase_time[1]
+      dummy <- the_cycle$o2_cordelta[1] / the_cycle$phase_time[1]
       units(output$slope_cor) <- units(dummy)
      
       return(output)
