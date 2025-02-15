d314a16d9d87f215ef3acb926b8281f330bee928
hugomflavio <hmflavio92@gmail.com>
2025-02-15 11:20:55 -0400

    make melt_resp work directly with resp input

--- R/melt_resp.R
+++ R/melt_resp.R
@@ -1,18 +1,22 @@
 #' Convert a human-friendly respirometry table to a computer-friendly format
-#' 
+#'
 #' Also appends additional probe info if provided.
-#' 
-#' @param input A dataframe with oxygen, temperature, pressure, and phase data.
-#'  The output of \code{\link{merge_pyro_phases}}
-#' @inheritParams load_experiment
-#' 
+#'
+#' @param input A resp list containing a phased object.
+#'
 #' @export
-#' 
-melt_resp <- function(input, probe_info) {
+#'
+melt_resp <- function(input) {
 
-  ox_aux <- reshape2::melt(input, 
+  if (is.null(input$phased)) {
+    stop("Couldn't find object 'phased' inside input.",
+         " Have you run assign_phases?", call. = FALSE)
+  }
+  phased <- input$phased
+
+  ox_aux <- reshape2::melt(phased,
     id.vars = "date_time",
-    measure.vars = colnames(input)[grepl("ox_", colnames(input))],
+    measure.vars = colnames(phased)[grepl("ox_", colnames(phased))],
     value.name = "o2",
   )
   ox_aux$probe <- stringr::str_extract(ox_aux$variable, "(?<=ox_).*")
@@ -20,33 +24,33 @@ melt_resp <- function(input, probe_info) {
   colnames(ox_aux)[1] <- "date_time"
   ox_aux <- ox_aux[, c("date_time", "probe", "o2")]
 
-  pressure_aux <- reshape2::melt(input, 
+  pressure_aux <- reshape2::melt(phased,
     id.vars = "date_time",
-    measure.vars = colnames(input)[grepl("pressure_", colnames(input))],
+    measure.vars = colnames(phased)[grepl("pressure_", colnames(phased))],
     value.name = "pressure"
   )
   pressure_aux$variable <- NULL
   pressure_aux$date_time <- NULL
 
-  temp_aux <- reshape2::melt(input, 
+  temp_aux <- reshape2::melt(phased,
     id.vars = "date_time",
-    measure.vars = colnames(input)[grepl("temp_", colnames(input))],
+    measure.vars = colnames(phased)[grepl("temp_", colnames(phased))],
     value.name = "temp"
   )
   temp_aux$variable <- NULL
   temp_aux$date_time <- NULL
 
-  sal_aux <- reshape2::melt(input, 
+  sal_aux <- reshape2::melt(phased,
     id.vars = "date_time",
-    measure.vars = colnames(input)[grepl("sal_", colnames(input))],
+    measure.vars = colnames(phased)[grepl("sal_", colnames(phased))],
     value.name = "sal"
   )
   sal_aux$variable <- NULL
   sal_aux$date_time <- NULL
 
-  phase_aux <- reshape2::melt(input, 
+  phase_aux <- reshape2::melt(phased,
     id.vars = "date_time",
-    measure.vars = colnames(input)[grepl("phase_", colnames(input))],
+    measure.vars = colnames(phased)[grepl("phase_", colnames(phased))],
     value.name = "phase"
   )
   phase_aux$variable <- NULL
@@ -54,53 +58,24 @@ melt_resp <- function(input, probe_info) {
 
   pre_output <- cbind(ox_aux, pressure_aux, temp_aux, sal_aux, phase_aux)
 
-  if (any(grepl("ph_", colnames(input)))) {
-    pre_output$ph <- as.vector(t(input[, grepl("ph_", colnames(input))]))
+  if (any(grepl("ph_", colnames(phased)))) {
+    pre_output$ph <- as.vector(t(phased[, grepl("ph_", colnames(phased))]))
   }
 
   pre_output$cycle <- as.numeric(sub("F|M", "", as.character(pre_output$phase)))
 
   # if fish information is provided
-  if (!is.null(probe_info)) {
-    # discard any data that does not match the probes we want.
-    pre_output <- pre_output[pre_output$probe %in% probe_info$probe, ]
-
-    if (nrow(pre_output) == 0) {
-      stop("The probes listed in probe_info don't match any probe listed on ",
-           "the data. Aborting.")
-    }
-
-    # Then include the extra information
-    link <- match(pre_output$probe, probe_info$probe)
-    pre_output <- cbind(pre_output, probe_info[link, c("id", "mass", "volume")])
-  
-    # and trim away the cycles that happen before the first_cycle
-    by_probe <- split(pre_output, pre_output$probe)
-    
-    recipient <- lapply(names(by_probe), function(the_probe, probe_info) {
-
-      trimmed_db <- by_probe[[the_probe]]
-
-      first_cycle <- probe_info$first_cycle[probe_info$probe == the_probe]
-      if (!is.na(first_cycle)) {
-        trimmed_db <- trimmed_db[trimmed_db$cycle >= first_cycle, ]
-      } else {
-        trimmed_db <- NULL
-      }
-      
-      return(trimmed_db) 
-
-    }, probe_info = probe_info)
-
-    output <- as.data.frame(data.table::rbindlist(recipient))
+  if (!is.null(input$probe_info)) {
+    # Include the extra information
+    link <- match(pre_output$probe, input$probe_info$probe)
+    output <- cbind(pre_output,
+                    input$probe_info[link, c("id", "mass", "volume")])
   } else {
     output <- as.data.frame(pre_output)
   }
 
   output$phase <- as.character(output$phase)
 
-  return(output)
+  input$melted <- output
+  return(input)
 }
-
-
-
