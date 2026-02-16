ee7ccc9970574e95842ecabecce8fa493c0ac7b1
Hugo Flávio <40389021+hugomflavio@users.noreply.github.com>
2026-02-16 10:50:16 -0400

    comment out debug line

--- R/smr_functions.R
+++ R/smr_functions.R
@@ -28,7 +28,7 @@ calc_smr <- function(mr, G = 1:4, q = c(0.2, 0.25), p = 0.1, n = 10){
   issue_low_n_warning <- TRUE
 
   recipient <- lapply(by_probe, function(the_probe) {
-    cat(the_probe$probe[1], "\n")
+    # cat(the_probe$probe[1], "\n")
     if ("animal_mass" %in% colnames(the_probe)) {
       output <- the_probe[1, c("probe", "id", "animal_mass")]
       the_probe$input_mr <- the_probe$mr_g
